bkchr/proc-macro-crate

commit c2a4c9138b2ab0ab8c63a895d62d261b5a9f2f9a

Also walk `target.'cfg()'` for `dev-dependencies` next to `dependencies`
In [RustAudio/cpal#641] we're seeing a [failure in `ndk-macro`] to turn
a crate name with dashes into underscores as fallback, with the
underlying issue that `proc-macro-crate` failed to find our crate in
`Cargo.toml` when limited to a specific target cfg.  According to [the
Rust docs] it is perfectly possible to have `dev-dependencies` behind
such a target predicate too.

[RustAudio/cpal#641]: https://github.com/RustAudio/cpal/pull/641#issuecomment-1040470392
[failure in `ndk-macro`]: https://github.com/rust-windowing/android-ndk-rs/blob/9060d5dbea8bdd9aee9e42ef8e8375c35adbc271/ndk-macro/src/helper.rs#L64
[the Rust docs]: https://doc.rust-lang.org/cargo/reference/specifying-dependencies.html#development-dependencies

src/lib.rs
@@ -181,7 +181,8 @@ fn extract_crate_name(
         .and_then(|t| {
             t.values()
                 .filter_map(|v| v.as_table())
-                .filter_map(|t| t.get("dependencies").and_then(|t| t.as_table()))
+                .filter_map(|t| t.get("dependencies").or_else(|| t.get("dev-dependencies")))
+                .filter_map(|t| t.as_table())
                 .find_map(|t| extract_crate_name_from_deps(orig_name, t.clone()))
         })
     {

commit 05c653d888fc3e36b9017479c49705d1085f7f49

Fix `dev-dependencies` scan if `target.XYZ` clause also has `dependencies`
In the event that a `dependencies` table exists in addition to the
`dev-dependencies` table with the same `target.XYZ` clause, only the
`dependencies` table would get read when we instead desire both.

Fixes: c2a4c91 ("Also walk `target.'cfg()'` for `dev-dependencies` next to `dependencies`")

src/lib.rs
@@ -181,7 +181,8 @@ fn extract_crate_name(
         .and_then(|t| {
             t.values()
                 .filter_map(|v| v.as_table())
-                .filter_map(|t| t.get("dependencies").or_else(|| t.get("dev-dependencies")))
+                .flat_map(|t| [t.get("dependencies"), t.get("dev-dependencies")])
+                .flatten()
                 .filter_map(|t| t.as_table())
                 .find_map(|t| extract_crate_name_from_deps(orig_name, t.clone()))
         })